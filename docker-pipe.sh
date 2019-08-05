57a5bae6319dff5d815e5f8fdb1fe41de8ba8028
BhumishShah <meetbhumish@gmail.com>
2019-08-05 17:53:38 +0530

    Update docker-pipe.sh

--- docker-pipe.sh
+++ docker-pipe.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ssh root@172.31.35.197 <<EOF
 cd /home/ubuntu/mean
-docker build --rm -f Dockerfile -t mean:v1 mean
+docker build --rm -f Dockerfile -t mean:v1
 service docker stop
 service docker start
 docker run --rm -d -p 81:81 mean:v1
